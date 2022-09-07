86471e2a5d830c186c58fe7dc89fca6157fc9482
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2022-09-07 13:56:19 +0200

    firebase CRUD

--- MyApplication3/app/src/main/java/com/example/myapplication/CarAdapter.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/CarAdapter.java
@@ -1,15 +1,23 @@
 package com.example.myapplication;
 
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.ArrayList;
 
@@ -17,24 +25,30 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.ViewHolder> {
     private ArrayList<Car> CarArr;
     private Context context;
     PopupWindow popupadd;
-    public CarAdapter(ArrayList<Car> CarArr, Context context) {
+    AlertDialog.Builder builderdelete,builderedit,builderoption;
+    DatabaseReference myRef;
+    EditText editcarname,editcarmodel,editcarnumber;
+Button editcar,deletecar,editeditcar;
+    public CarAdapter(ArrayList<Car> CarArr, Context context,DatabaseReference myRef) {
         this.CarArr = CarArr;
         this.context = context;
+        this.myRef=myRef;
     }
+
     public ArrayList<Car> getCarArr(){
         return CarArr;
     }
     public Context getContext(){
         return context;
     }
-
+public DatabaseReference getMyRef(){return myRef;}
     public void setCarArr(ArrayList<Car> carArr) {
         CarArr = carArr;
     }
-
     public void setContext(Context context) {
         this.context = context;
     }
+public void setMyRef(DatabaseReference myRef){this.myRef=myRef;}
 
     @NonNull
     @Override
@@ -71,18 +85,139 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.ViewHolder> {
     itemView.setOnLongClickListener(new View.OnLongClickListener() {
         @Override
         public boolean onLongClick(View view) {
-            Intent intent=new Intent(context,firebase.class);
-            intent.putExtra("position",getAdapterPosition());
-            intent.putExtra("model",CarArr.get(getAdapterPosition()).getCarmodel());
-            intent.putExtra("name",CarArr.get(getAdapterPosition()).getCarname());
-            intent.putExtra("object",CarArr.get(getAdapterPosition()).getCarobject());
-            intent.putExtra("number",CarArr.get(getAdapterPosition()).getCarnumber());
 
-            context.startActivity(intent);
+
+            LayoutInflater ret = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View htfg = ret.inflate(R.layout.popupeditfirebase, null);
+
+            LayoutInflater rtr = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View etw = rtr.inflate(R.layout.optionpopup, null);
+
+
+            editcar=etw.findViewById(R.id.editcarbtn);
+            deletecar=etw.findViewById(R.id.deletecarbtn);
+
+            editcarname=htfg.findViewById(R.id.editcarname);
+            editcarnumber=htfg.findViewById(R.id.editcarnumber);
+            editcarmodel=htfg.findViewById(R.id.editcarmodel);
+            editeditcar=htfg.findViewById(R.id.editcarfirebase);
+
+
+            builderoption = new AlertDialog.Builder(context);
+            builderoption.setView(etw);
+            AlertDialog alert = builderoption.create();
+            alert.show();
+
+
+
+            editcar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //        popup tany
+                    editcarname.setText(CarArr.get(getAdapterPosition()).getCarname());
+                    editcarmodel.setText(String.valueOf(CarArr.get(getAdapterPosition()).getCarmodel()));
+                    editcarnumber.setText(String.valueOf(CarArr.get(getAdapterPosition()).getCarnumber()));
+
+
+                    builderedit = new AlertDialog.Builder(context);
+                    builderedit.setView(htfg);
+                    AlertDialog alert3 = builderedit.create();
+                    alert3.show();
+
+
+                    editeditcar.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Car n= new Car(editcarname.getText().toString().trim(), Integer.parseInt(editcarmodel.getText().toString().trim()),Integer.parseInt(editcarnumber.getText().toString().trim()),CarArr.get(getAdapterPosition()).getCarobject());
+                            CarArr.add(n);
+                            writeNewCarobj(n);
+                            notifyItemInserted(CarArr.size());
+
+                            alert3.dismiss();
+                            alert.dismiss();
+                            Toast.makeText(context, "Car edited", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    });
+
+                }
+            });
+
+
+            deletecar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    builderdelete = new AlertDialog.Builder(context);
+
+            builderdelete.setMessage("Are you sure you want to delete this?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            myRef.child(CarArr.get(getAdapterPosition()).getCarobject()).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+//                                    CarArr.remove(getAdapterPosition());
+//                                    notifyItemRemoved(getAdapterPosition()); //could remove
+                                    Toast.makeText(context,"item deleted successfully",
+                                            Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                    alert.dismiss();
+                                }
+                            });
+//
+//                            Toast.makeText(context,"you choose yes action for alertbox",
+//                                    Toast.LENGTH_SHORT).show();
+
+////                            popupoption.dismiss();
+//                            context.finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+                            dialog.cancel();
+                            Toast.makeText(context,"you choose no action for alertbox",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+            AlertDialog alert2 = builderdelete.create();
+            alert2.show();
+                }
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            Intent intent=new Intent(view.getContext(),firebase.class);
+//            intent.putExtra("position",getAdapterPosition());
+//            intent.putExtra("model",CarArr.get(getAdapterPosition()).getCarmodel());
+//            intent.putExtra("name",CarArr.get(getAdapterPosition()).getCarname());
+//            intent.putExtra("object",CarArr.get(getAdapterPosition()).getCarobject());
+//            intent.putExtra("number",CarArr.get(getAdapterPosition()).getCarnumber());
+//            view.getContext().startActivity(intent);
+
+
 
 //            CarArr.remove(getAdapterPosition());
-//
 //            notifyItemRemoved(getAdapterPosition());
+
+
 
             return true;
         }
@@ -95,5 +230,9 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.ViewHolder> {
         }
 
 
+    }
+
+    public void writeNewCarobj(Car car) {
+        myRef.child(car.getCarobject()).setValue(car);
     }
 }
--- MyApplication3/app/src/main/java/com/example/myapplication/firebase.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/firebase.java
@@ -1,10 +1,8 @@
 package com.example.myapplication;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,17 +32,18 @@ public class firebase extends AppCompatActivity {
     FirebaseDatabase mDatabase;
     DatabaseReference myRef;
     static ArrayList<Car> c;
-    PopupWindow popupadd,popupobtion;
+    PopupWindow popupadd;
     RelativeLayout linearLayout1;
     ArrayList<Car> c1;
     Button addcar;
-    Bundle b;
     EditText carname,carmodel,carnumber,carobject;
     private RecyclerView carrec;
-FloatingActionButton add;
+    FloatingActionButton add;
     // variable for our adapter class and array list
     private CarAdapter caradapter;
     private static final String TAG = "";
+
+
 
 
 
@@ -63,46 +62,18 @@ FloatingActionButton add;
         popupadd = new PopupWindow(addpopup, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
 
-        LayoutInflater tytr = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View optionpopup = tytr.inflate(R.layout.optionpopup,null);
-        popupobtion = new PopupWindow(optionpopup, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-         linearLayout1=findViewById(R.id.relativefirebase);
-        c=new ArrayList<>();
-        addcar=addpopup.findViewById(R.id.addcarfirebase);
-        c1=new ArrayList<>();
-        add.show();
         carname=addpopup.findViewById(R.id.addcarname);
         carmodel=addpopup.findViewById(R.id.addcarmodel);
         carnumber=addpopup.findViewById(R.id.addcarnumber);
         carobject=addpopup.findViewById(R.id.addcarobject);
-        Intent intent = getIntent();
-         b=intent.getExtras();
 
-//int position=(int)getIntent().getSerializableExtra("position");
-        if (b!=null){
-            int position=b.getInt("position");
-            int model=b.getInt("model");
-            String name=b.getString("name");
-            String object=b.getString("object");
-            int number=b.getInt("number");
+        linearLayout1=findViewById(R.id.relativefirebase);
+        c=new ArrayList<>();
+        addcar=addpopup.findViewById(R.id.addcarfirebase);
+        c1=new ArrayList<>();
+        add.show();
 
-            popupobtion.showAtLocation(linearLayout1, Gravity.CENTER, 0, 0);
-            popupobtion.setFocusable(true);
 
-            popupobtion.update();
-
-//Car sdfs= (Car) b.get("car");
-            Log.i("DFsdgf=================sdgsde","position"+position);
-            Log.i("DFsdg================fsdgsde","position"+model);
-            Log.i("DFsdgf===================sdgsde","position"+name);
-            Log.i("DFsdgf================sdgsde","position"+object);
-            Log.i("DFsdg==================fsdgsde","position"+number);
-//            c=caradapter.getCarArr();
-//            Car sdfs=   c.get(position);
-//            Log.i("DFsdgfsdg=======================sde","object"+sdfs.getCarobject());
-
-        }
 add.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
@@ -120,12 +91,10 @@ add.setOnClickListener(new View.OnClickListener() {
 addcar.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
-        Car n= new Car(carname.getText().toString(), Integer.parseInt(carmodel.getText().toString()),Integer.parseInt(carnumber.getText().toString()),carobject.getText().toString());
+        Car n= new Car(carname.getText().toString().trim(), Integer.parseInt(carmodel.getText().toString().trim()),Integer.parseInt(carnumber.getText().toString().trim()),carobject.getText().toString().trim());
         c.add(n);
         writeNewCarobj(n);
         caradapter.notifyItemInserted(c.size());
-//        caradapter.notifyItemInserted();
-       Log.i("dsfsfgfgfgfgfgfgfgfgfgfgd","sdfsd"+carrec.getChildAdapterPosition(view)) ;
         popupadd.dismiss();
         Toast.makeText(firebase.this, "Car added", Toast.LENGTH_SHORT).show();
 
@@ -142,25 +111,25 @@ addcar.setOnClickListener(new View.OnClickListener() {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 c=new ArrayList<>();
                 for (DataSnapshot child : dataSnapshot.getChildren()) {
-                        Log.i("fsdfwesfwee","ewfsd key="+child.getKey());
-                    Log.i("fsdfwesfwee","ewfsd value="+child.getValue());
+//                        Log.i("fsdfwesfwee","ewfsd key="+child.getKey());
+//                    Log.i("fsdfwesfwee","ewfsd value="+child.getValue());
                     Car message = child.getValue(Car.class);
 
                             c.add(message);
 
 
-                    Log.i("fsdfwesfwee","ewfsd object="+message.getCarobject());
-                    Log.i("fsdfwesfwee","ewfsd number="+message.getCarnumber());
-                    Log.i("oilo9888888888", "name = " + message.getCarname());
-                    Log.i(TAG, "jhjjjjjjj67676767676767 model= " + message.getCarmodel());
+//                    Log.i("fsdfwesfwee","ewfsd object="+message.getCarobject());
+//                    Log.i("fsdfwesfwee","ewfsd number="+message.getCarnumber());
+//                    Log.i("oilo9888888888", "name = " + message.getCarname());
+//                    Log.i(TAG, "jhjjjjjjj67676767676767 model= " + message.getCarmodel());
                 }
                 buildRecyclerView(c);
 
                 String path = dataSnapshot.getRef().toString();
                 long count = dataSnapshot.getChildrenCount();
-                System.out.println("-----------------------------"+dataSnapshot.getValue().toString());
-//                System.out.println("--------------"+path);
-                System.out.println("--------------"+count);
+//                System.out.println("-----------------------------"+dataSnapshot.getValue().toString());
+////                System.out.println("--------------"+path);
+//                System.out.println("--------------"+count);
             }
 
 
@@ -180,22 +149,17 @@ addcar.setOnClickListener(new View.OnClickListener() {
 //        myRef.child("car3").child("carnumber").setValue(car.getCarnumber());
 //        myRef.child("car3").child("carname").setValue(car.getCarname());
         myRef.child(car.getCarobject()).setValue(car);
-//        mUserRef.child("car2").child("carname").setValue(car.getCarname());
-        Log.e("=======================", "Clicked on add!");
+//        Log.e("=======================", "Clicked on add!");
     }
 
     public void writeNewCarobj(Car car) {
-//        Car car = new Car(carobject,carmodel, carnumber,carname);
-//        myRef.child("car3").child("carmodel").setValue(car.getCarmodel());
-//        myRef.child("car3").child("carnumber").setValue(car.getCarnumber());
-//        myRef.child("car3").child("carname").setValue(car.getCarname());
+//
         myRef.child(car.getCarobject()).setValue(car);
-//        mUserRef.child("car2").child("carname").setValue(car.getCarname());
-        Log.e("=================", "Clicked on add!");
+//        Log.e("=================", "Clicked on add!");
     }
     private void buildRecyclerView(ArrayList<Car> cerw) {
         // initializing our adapter class.
-        caradapter = new CarAdapter(cerw, this);
+        caradapter = new CarAdapter(cerw, this,myRef);
 
         // adding layout manager to our recycler view.
         LinearLayoutManager manager = new LinearLayoutManager(this);
@@ -207,5 +171,7 @@ addcar.setOnClickListener(new View.OnClickListener() {
         // setting adapter to our recycler view.
         carrec.setAdapter(caradapter);
     }
+
+
 
 }
